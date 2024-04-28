90e52bc86c0fed4e2a51b2aa49b1f1aaaf48e819
Mukul Chugh <mukulchughofficial@gmail.com>
2024-04-28 18:28:43 +0530

    Update email recipient in sendEmail.ts and adjust styling in project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -34,14 +34,14 @@ ProjectProps) {
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[44rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-0 px-2 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[100%] flex flex-col h-full ">
+      <section className="bg-gray-100 max-w-[44rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 md:p-4 relative sm:h-[15rem] hover:bg-gray-200 transition sm:group-even:pl-8 md:group-even:pl-4 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-0 px-2 sm:pl-10 sm:pr-2 sm:pt-10 md:p-0 sm:max-w-[100%] flex flex-col h-full">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
           {Array.isArray(tags) && (
-            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto md:mt-4">
               {tags.map((tag, index) => (
                 <li
                   className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
