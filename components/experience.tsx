ff229c9b586c67a6fc60fd737bd98ab755c699fd
Mukul Chugh <mukulchughofficial@gmail.com>
2024-04-28 16:04:08 +0530

    refactor

--- components/experience.tsx
+++ components/experience.tsx
@@ -18,7 +18,7 @@ export default function Experience() {
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline lineColor="">
+      {/* <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
@@ -58,7 +58,7 @@ export default function Experience() {
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
-      </VerticalTimeline>
+      </VerticalTimeline> */}
     </section>
   );
 }
