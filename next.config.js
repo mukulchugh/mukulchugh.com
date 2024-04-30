1390171e3ffb81a3d9581413a01e995869dee320
Mukul Chugh <mukulchughofficial@gmail.com>
2024-04-30 23:56:54 +0530

    Add cache control headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,23 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   optimizeFonts: true,
+
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate", // 1 year
+          },
+        ],
+      },
+    ];
+  },
   images: {
+    minimumCacheTTL: 60 * 60 * 24 * 365, // 1 year
     remotePatterns: [
       {
         protocol: "https",
